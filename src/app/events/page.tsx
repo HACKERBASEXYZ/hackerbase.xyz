2b9fd3d5f7ac270d3ec137e03dea7ee59f6a476b
Paul-Henry Kajfasz <kajfaszph@gmail.com>
2023-05-19 22:25:00 +0200

    updated layout / size / padding

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -14,7 +14,7 @@ const eventsPage = async () => {
           Events
         </h1>
       </div>
-      <div className="p-4 flex flex-col md:flex-row w-full">
+      <div className="px-10 md:px-20 lg:px-32 pt-4 flex flex-col md:flex-row w-full">
         <div className="w-1/4">
           <h1>Filter</h1>
         </div>
@@ -22,7 +22,7 @@ const eventsPage = async () => {
           <h1 className="border-2 border-black rounded mb-4 pl-2">
             🔍 Searchbar
           </h1>
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 lg:gap-8">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
             {events.map((event) => (
               <EventCard key={event.id} {...event} />
             ))}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,9 +11,9 @@ const home = async () => {
       <div className="h-96 w-full bg-blue-300 relative landingHeader">
         <Image alt="hackers" src={"/bg.jpg"} fill />
       </div>
-      <div className="p-4">
+      <div className="px-10 md:px-20 lg:px-32 pt-4">
         <h1 className="text-2xl mb-2">Featured events</h1>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 lg:gap-8">
+        <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-8">
           {featuredEvents.map((event) => (
             <EventCard key={event.id} {...event} />
           ))}
