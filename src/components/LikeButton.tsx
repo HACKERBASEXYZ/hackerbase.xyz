1175475d2b5a4ca99af85e899edbfd2120c54389
Paul-Henry Kajfasz <kajfaszph@gmail.com>
2023-05-20 11:57:45 +0200

    added like button to EventCard + added turbopack conf

--- src/components/LikeButton.tsx
+++ src/components/LikeButton.tsx
@@ -1,7 +1,16 @@
+"use client";
+
+import { AiOutlineHeart } from "react-icons/ai";
+
 const LikeButton: React.FC = () => {
   return (
-    <div>
-      <h1>Likebutton</h1>
-    </div>
+    <button
+      className="z-10 rounded-full border-gray-300 border inline-block p-2 bg-white shadow-lg absolute top-1/2 right-2 -translate-y-1/2"
+      onClick={() => console.log("Liked event")}
+    >
+      <AiOutlineHeart size={22} className="hover:text-red-500" />
+    </button>
   );
 };
+
+export default LikeButton;
